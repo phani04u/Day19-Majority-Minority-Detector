41886f307cbdb1217753aa9d0e7ee398423a1070
phani04u <singudasuphaneendra@gmail.com>
2026-07-30 22:37:30 +0530

    Add files via upload

---
+++ majaority_dector/majaority_dector.sim/sim_1/behav/simulate.bat
@@ -0,0 +1,9 @@
+@echo off
+set xv_path=D:\\XILINX\\Vivado\\2016.4\\bin
+call %xv_path%/xsim MAJ_TB_behav -key {Behavioral:sim_1:Functional:MAJ_TB} -tclbatch MAJ_TB.tcl -log simulate.log
+if "%errorlevel%"=="0" goto SUCCESS
+if "%errorlevel%"=="1" goto END
+:END
+exit 1
+:SUCCESS
+exit 0
